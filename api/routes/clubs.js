39987d37c21ddce7c0df043fbd26f5792d3cd4c1
Carin H Brander <hej@carinh.se>
2021-11-23 12:48:56 +0100

    Buncha stuff

--- api/routes/clubs.js
+++ api/routes/clubs.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const NodeGeocoder = require('node-geocoder');
+const options = {
+  provider: 'google',
+  apiKey: 'AIzaSyBHEvEc7YtP0yIN3AvSUj-Fdvl-ZEnnKB0', // for Mapquest, OpenCage, Google Premier
+  formatter: null // 'gpx', 'string', ...
+};
+const geocoder = NodeGeocoder(options);
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('clubs!');
+router.get('/', async function(req, res, next) {
+    var data = [{
+        name: 'Hello',
+        website: 'https://carinh.se',
+        city: 'Järfälla',
+        location: await geocoder.geocode('järfälla')
+    },
+    {
+      name: 'Hello',
+      website: 'https://carinh.se',
+      city: 'Järfälla',
+      location: await geocoder.geocode('göteborg')
+  }
+  ]
+  res.json(data);
 });
 
 module.exports = router;
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
+import * as d3 from "d3";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.chartElement = React.createRef();
+    this.state = {
+      clubs: [],
+      data: [4, 8, 15, 16, 23, 42]
+    };
+  }
+
+  callAPI() {
+    axios.get(`http://localhost:1337/clubs`)
+      .then(res => {
+        this.setState({ clubs: res.data });
+        this.createChart();
+      })
+  }
+
+  createChart() {
+
+    //Get Sweden map
+    axios.get(`https://raw.githubusercontent.com/perliedman/svenska-landskap/master/svenska-landskap.geo.json`)
+      .then(res => {
+        var el = this.chartElement.current;
+
+        let context = d3.select(el);
+
+        let projection = d3.geoMercator().fitExtent([[0, 0], [1000, 1000]], res.data);
+
+        let geoGenerator = d3.geoPath().projection(projection);
+
+        context.selectAll('path')
+            .data(res.data.features)
+            .join('path')
+            .attr('d', geoGenerator);
+
+        context.selectAll("circle")
+        .data(this.state.clubs)
+        .enter()
+        .append("circle")
+        .attr("r", "5")
+        .attr("fill", "red")
+        .attr("transform", function(d) {
+          return "translate(" + projection([d.location[0].longitude, d.location[0].latitude]) + ")";
+        })
+      })
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div >
+        <svg width="1000" height="1000">
+          <g ref={this.chartElement}></g>
+        </svg>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
